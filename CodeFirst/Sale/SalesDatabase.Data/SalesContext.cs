93ea534384738abd00a008aba24006b0beb0b7dd
Sleya <yehiazz98246@abv.bg>
2019-01-03 16:33:33 +0200

    Added DbSets to tables

--- CodeFirst/Sale/SalesDatabase.Data/SalesContext.cs
+++ CodeFirst/Sale/SalesDatabase.Data/SalesContext.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesDatabase.Data.EntityConfig;
+using SalesDatabase.Data.Models;
 using System;
 
 namespace SalesDatabase.Data
 {
     public class SalesContext : DbContext
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Store> Stores { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
