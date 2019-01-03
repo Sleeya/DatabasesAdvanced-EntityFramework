def0f23dde816c3be691c9e49f1f96406f575170
Sleya <yehiazz98246@abv.bg>
2019-01-03 02:17:48 +0200

    Uploading Hospital solution

---
+++ CodeFirst/Hospital/P01_HospitalDatabase.Data.Models/Patient.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Patient
+    {
+
+        public Patient()
+        {
+            this.Prescriptions = new List<PatientMedicament>();
+            this.Diagnoses = new List<Diagnose>();
+            this.Visitations = new List<Visitation>();
+        }
+        public int PatientId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Addreess { get; set; }
+
+        public string Email { get; set; }
+
+        public bool hasInsurance { get; set; }
+
+        public ICollection<PatientMedicament> Prescriptions { get; set; }
+
+        public ICollection<Diagnose> Diagnoses { get; set; }
+
+        public ICollection<Visitation> Visitations { get; set; }
+
+    }
+}
---
+++ CodeFirst/Hospital/P01_HospitalDatabase.Data/Confiugartion.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace P01_HospitalDatabase.Data
+{
+   internal class Confiugartion
+    {
+        public const string ConfigurationString = "Server = YEHIAZZ-PC\\SQLEXPRESS; Database = Hospital; Integrated Security = True;";
+    }
+}
